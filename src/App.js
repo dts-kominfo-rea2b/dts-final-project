3f225a4adf7ae4aacd57ceba64a1112cc6e20cd2
Budianto IP <budiantoip@gmail.com>
2022-08-03 09:00:47 +0700

    add top section

--- src/App.js
+++ src/App.js
@@ -4,16 +4,26 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from './containers/HomePage';
 import LoginPage from './containers/LoginPage';
 import RegisterPage from './containers/RegisterPage';
+import { AnimatePresence } from "framer-motion";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-    <div className='App'>
-      <Routes>
-        <Route path='/*' element={<HomePage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/register' element={<RegisterPage />} />
-      </Routes>
-    </div>
+    <AnimatePresence exitBeforeEnter>
+      <ToastContainer />
+      <div className='w-screen h-auto min-h-[100vh] flex flex-col bg-primary'>
+        <main
+          className={`'mt-16 md:mt-16 px-3 md:px-8 md:py-6 py-4'} w-full h-auto`}
+          onClick={() => {}}
+        >
+          <Routes>
+            <Route path='/*' element={<HomePage />} />
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/register' element={<RegisterPage />} />
+          </Routes>
+        </main>
+      </div>
+    </AnimatePresence>
   );
 }
 
--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,5 +1,11 @@
+import Showcase from "../components/Showcase/Showcase";
+
 const HomePage = () => {
-  return <div></div>;
+  return (
+    <div className='flex w-full h-auto flex-col items-center justify-center'>
+      <Showcase />
+    </div>
+  );
 };
 
 export default HomePage;
