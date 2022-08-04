2a0591343549600d723465c5459c04e832344794
bochill <dev.hafidhf@gmail.com>
2022-08-04 15:51:37 +0700

    Update rawg.js

--- src/apis/rawg.js
+++ src/apis/rawg.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const rawg = axios.create({
     baseURL: 'https://api.rawg.io/api/games',
     params: {
-        key : 'b3e8ba01ddd44d1d8507fe2dd8d95b2d',
+        key : API_KEY,
     }
 });
 
