2313174840aeb2491b86accac90971bd35483a7d
gustiadiputra <106220424+gustiadiputra@users.noreply.github.com>
2022-08-05 08:26:43 +0700

    Update corousel.js

--- src/components/layout/corousel.js
+++ src/components/layout/corousel.js
@@ -1,61 +1,45 @@
-import React from "react";
-import { useState , useEffect} from "react";
+import { useState, useEffect } from "react";
+import axios from 'axios';
+import Carousel from 'better-react-carousel'
+
+
+// styles
+import styles from "./corousel.css";
 
 
 
-const Gallery = (url) => {
+const UncontrolledExample = () => {
   const [bookList,setList]=useState([]);
-  const [data, setData] = useState(null);
-  const [isPending, setIsPending] = useState(false);
-  const [error, setError] = useState(null);
-  const [items, setItems] = useState([]);
-
-  const [pageCount, setpageCount] = useState(0);
     useEffect(() => {
-    const controller = new AbortController();
-    const { signal } = controller;
-    setData(null);
-    setIsPending(true);
-    setError(null);
-
-    async function fetchData() {
-      try {
-        const response = await fetch(url, {
-          signal,
-          headers: {
-            "x-rapidapi-host": process.env.REACT_APP_API_HOST,
-            "x-rapidapi-key": process.env.REACT_APP_API_KEY,
-          },
+        axios.get('https://api.rawg.io/api/games?key=c081ece3d564465e863515d149c45031')
+        .then(res=>setList(res.data.results))
+        .catch(err=>console.log(err));
         });
-        if (!response.ok) {
-          throw new Error("Could not fetch the data.");
-        }
-        const result = await response.json();
-        const total = response.headers.get("x-total-count");
-        setItems(result);
-        setData(result);
-        setIsPending(false);
-        setError(null);
-        console.log(result)
-      } catch (error) {
-        setIsPending(false);
-        if (error.name === "AbortError") {
-          console.log(error.message);
-          setError(error.message);
-        } else {
-          setError(error.message);
-        }
-      }
-    }
-    fetchData();
-    return () => controller.abort();
-  }, );
-      return (
-        <>
-            
-                
 
+    console.log(bookList);
+      return (
+        <>  
+          <section>
+            <div className="text-center">
+           
+            </div> 
+            <br></br>
+                {bookList.length > 0 && (
+                    <Carousel cols={2} rows={1} gap={10} loop>
+                        {bookList.map(user => (
+                          <Carousel.Item>
+                            <img src={user.background_image}  alt={user.name} className="img" />
+                          </Carousel.Item>
+                                                
+                        ))}
+                    </Carousel>
+                )}
+                <section className={styles.games_content}>
+                    
+                </section>         
+          </section>
         </>
       )
 }
-export default Gallery;
+
+export default UncontrolledExample;
